a9cf1d44cd819f0a40d512c2efa5a643cf9d07a5
moezovic <16556866+moezovic@users.noreply.github.com>
2019-06-05 15:59:57 +0200

    Finish testing the security Controller

--- tests/SecurityControllerTest.php
+++ tests/SecurityControllerTest.php
@@ -6,12 +6,39 @@ use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
 
 class SecurityControllerTest extends WebTestCase
 {
-    public function testSomething()
+    public function testValidCredentials()
     {
         $client = static::createClient();
-        $crawler = $client->request('GET', '/');
+        $crawler = $client->request('GET', '/login');
 
-        $this->assertSame(200, $client->getResponse()->getStatusCode());
-        $this->assertContains('Hello World', $crawler->filter('h1')->text());
+        $form = $crawler->selectButton('Connexion')->form();
+
+        $form['_username'] = 'admin@admin.com';
+        $form['_password'] = 'pass_1234';
+
+        $crawler = $client->submit($form);
+        $crawler = $client->followRedirect();
+       
+        $this->assertEquals(
+          1, $crawler->filter('li:contains("Nouvelle figure")')->count()
+        );
+    }
+
+    public function testWrongCredentials()
+    {
+        $client = static::createClient();
+        $crawler = $client->request('GET', '/login');
+
+        $form = $crawler->selectButton('Connexion')->form();
+
+        $form['_username'] = 'wrong@wrong.com';
+        $form['_password'] = 'mkgfg87';
+
+        $crawler = $client->submit($form);
+        $crawler = $client->followRedirect();
+       
+        $this->assertEquals(
+          1, $crawler->filter('span:contains("Identifiants invalides.")')->count()
+        );
     }
 }
