c43b942d5e8a48a67d1a4f52748af45b3b8a23d2
moezovic <16556866+moezovic@users.noreply.github.com>
2019-06-14 17:05:23 +0200

    cretae a file upload management service: used in datafixtures and trickController

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -5,24 +5,30 @@ use App\Entity\Trick;
 use App\Entity\User;
 use App\Entity\Comment;
 
+use App\Service\UploadedFileManager;
+
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Common\Persistence\ObjectManager;
 
 use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
+use Symfony\Component\HttpFoundation\File\UploadedFile;
+use Symfony\Component\HttpFoundation\File\File;
 
 
 class AppFixtures extends Fixture
 {
 
   private $encoder;
+  private $uploadedFile;
 
-  public function __construct(UserPasswordEncoderInterface $encoder )
+  public function __construct(UserPasswordEncoderInterface $encoder, UploadedFileManager $uploadedFile, $rootDirectory )
     {
         $this->encoder = $encoder;
+        $this->uploadedFile = $uploadedFile;
+        $this->rootDirectory = $rootDirectory;
     }
     public function load(ObjectManager $manager)
     {
-        
         $user = new User();
         $user->setNickName('admin');
 
@@ -34,13 +40,19 @@ class AppFixtures extends Fixture
         $manager->persist($user);
         $manager->flush();
 
-        for ($i=0; $i < 30 ; $i++) { 
+
+        for ($i=0; $i < 10 ; $i++) { 
           $trick = new Trick();
-          $trick->setName("trick " . $i);
+          $trick->setName("trick " . ($i+1));
           $trick->setDescription("Le Lorem Ipsum est simplement du faux texte employé dans la composition et la mise en page avant impression. Le Lorem Ipsum est le faux texte standard de l'imprimerie depuis les années 1500, quand un imprimeur anonyme assembla ensemble des morceaux de texte pour réaliser un livre spécimen de polices de texte. Il n'a pas fait que survivre cinq siècles, mais s'est aussi adapté à la bureautique informatique, sans que son contenu n'en soit modifié. Il a été popularisé dans les années 1960 grâce à la vente de feuilles Letraset contenant des passages du Lorem Ipsum, et, plus récemment, par son inclusion dans des applications de mise en page de texte, comme Aldus PageMaker");
           $trick->setNiveau(mt_rand(1, 4));
           $trick->setTrickGroup(mt_rand(1, 3));
-          $trick->addImgDoc("4002d37a3879c2d63c8e6aed7ad31449.jpeg");
+          // $trick->addImgDoc("4002d37a3879c2d63c8e6aed7ad31449.jpeg");
+          $imageFile = [];
+
+          $imageFile[] = new File($this->rootDirectory .'/public/img/fixtures/img-'. ($i+1) .'.jpg','img-'. ($i+1) .'.jpg', 'image/jpeg', null, null, true);
+
+          $trick->setImgDocs($this->uploadedFile->docsInputManager($imageFile));
 
           for ($ii=0; $ii < 5; $ii++) { 
             $comment = new comment();
@@ -52,6 +64,7 @@ class AppFixtures extends Fixture
           }
 
           $manager->persist($trick);
+
         }
         $manager->flush();
     }
